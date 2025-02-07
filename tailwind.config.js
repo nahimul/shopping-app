d9ecb1bab02f1e259ae595fff6e73ec81ecd9f91
nahimul <nahimul404@gmail.com>
2025-02-08 02:53:01 +0600

    set up the project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,ts}" // Include all Angular HTML and TypeScript files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
